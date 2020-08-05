f836c03a89f456cb09f6902841d7ed3bda168b0e
Nastaran Heydari <nh.1374@yahoo.com>
2020-08-05 22:25:35 +0430

    necessary modules are imported

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,17 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCardModule} from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './components/customer-list/customer-list.component';
 import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
 import { CustomerOrdersComponent } from './components/customer-orders/customer-orders.component';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './main.component';
 
 
 @NgModule({
-  declarations: [CustomerListComponent, CustomerDetailsComponent, CustomerOrdersComponent, MainComponent],
+  declarations: [
+    MainComponent,
+    CustomerListComponent, 
+    CustomerDetailsComponent, 
+    CustomerOrdersComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatTooltipModule,
+    MatToolbarModule,
     CustomersRoutingModule
   ]
 })
