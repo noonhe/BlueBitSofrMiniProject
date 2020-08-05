e09ac9c8a9df5ba127bf193db0b5c7dd462ae1b2
Nastaran Heydari <nh.1374@yahoo.com>
2020-08-05 22:20:04 +0430

    routes added

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main.component';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
+import { CustomerOrdersComponent } from './components/customer-orders/customer-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:MainComponent,
+    children:[
+      {
+        path:'list',
+        component: CustomerListComponent
+      },
+      {
+        path:'customer/:id/detail',
+        component:CustomerDetailsComponent
+      },
+      {
+        path:'customer/:id/orders',
+        component:CustomerOrdersComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
