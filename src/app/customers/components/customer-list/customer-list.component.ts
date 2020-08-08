8af8bf35fe9a690066e6013d595cd5adfc45a24f
Nastaran Heydari <nh.1374@yahoo.com>
2020-08-08 23:46:44 +0430

    customer list component completed

--- src/app/customers/components/customer-list/customer-list.component.ts
+++ src/app/customers/components/customer-list/customer-list.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../../services/customer.service';
+import { ServiceResponse , Customer } from '../../Data/interfaces';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-customer-list',
+  selector: 'customer-list',
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.scss']
 })
 export class CustomerListComponent implements OnInit {
 
-  constructor() { }
+  customersList : Customer[] = [];
+  currentTabIndex = 0;
+  constructor(
+    private customersService : CustomerService,
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
+    this.customersService.getCustomers().subscribe(
+      (result: ServiceResponse) => {
+        if(result.resultCode === 0){
+          this.customersList = result.data;
+        }else {
+
+        }
+      },
+      error => {
+
+      }
+    )
   }
 
+  navigateToCustomerInfo(customer : Customer){
+    this.router.navigate([`/customers/customer-info/${customer.id}/details`]);
+  }
+
+  navigateToOrders(customer : Customer):void{
+    this.router.navigate([`/customers/customer-info/${customer.id}/orders`]);
+  }
 }
