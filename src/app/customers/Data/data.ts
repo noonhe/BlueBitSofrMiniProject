6101d3e351b36a3b2341a9ae3a5205f601ec87ef
Nastaran Heydari <nh.1374@yahoo.com>
2020-08-08 23:46:09 +0430

    mock data

---
+++ src/app/customers/Data/data.ts
@@ -0,0 +1,305 @@
+import { Customer } from "./interfaces";
+
+export var customerList : Customer[] = [
+    {
+        id:"1",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-male.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"2",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-female.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"3",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-male.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"4",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-male.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"5",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-male.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"6",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-male.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"7",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-male.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+
+        id:"8",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-female.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"9",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-female.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"10",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-male.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"11",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-female.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"12",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-male.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"13",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-female.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"14",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-male.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    },
+    {
+        id:"15",
+        firstName: 'Ted',
+        lastName: 'James',
+        avatar: '/assets/images/avatar-female.svg',
+        addressInfo:{
+            city: 'Phoenix',
+            state: 'Arizona',
+            address: '1234 Anywhere street'
+        },
+        order:{
+            orderTotal: 30,
+            items:[
+                {
+                    title:'shows',
+                    price: 30
+                }
+            ]
+        }
+    }
+]
